3bc1cc9e7e12d8d4419b29b040fe596e4ff695d2
Tony Tribelli <tony.tribelli@gmail.com>
2025-06-02 12:45:35 -0700

    Added some experimental MIDR code for embedded environments

---
+++ midr.h
@@ -0,0 +1,19 @@
+// midr.h
+
+#ifndef midr_h
+#define midr_h
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    uint32_t get_midr(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // midr_h
+
