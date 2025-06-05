b43a68532672246ba023e4f6d7753810c9d04530
Tony Tribelli <ttribelli@perpenso.com>
2025-06-05 16:45:20 -0700

    More refactoring

--- cpuinfo.c
+++ cpuinfo.c
@@ -95,7 +95,7 @@
 
 
 
-// -------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 // Constants for conditional compilation
 
 #if defined(_M_X64) || defined(__x86_64__)  // 64-bit Intel
@@ -130,7 +130,14 @@
 
 
 
-// -------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// Number of elements in an array
+
+#define elementsof(array) (sizeof(array) / sizeof(array[0]))
+
+
+
+// ----------------------------------------------------------------------------
 // Let's be paranoid about string termination
 
 #define strterm(buf, len) ((buf)[(len)       - 1] = 0)
@@ -140,19 +147,18 @@
 
 #if ANY_ARM
 
-// -------------------------------------------------------------------------
-// Lookup table for ARM implementors and parts.
-// Use -1 to indicate no entry. For example a -1 for the part field
-// indicates an implementor name. -1's for both the imp and part fields
-// indicate the end of the table.
+// ----------------------------------------------------------------------------
+// Lookup table for ARM implementers and parts.
+// Use -1 to indicate no entry. For example a -1 for the part number field
+// indicates an implementer name, otherwise it is a part name.
 // https://github.com/bp0/armids
 
 static struct {
-    int         imp;
-    int         part;
+    int         implementer;
+    int         partnum;
     const char *name;
 } part_info[] = {
-    0x41,    -1, "ARM",                     // Implementor
+    0x41,    -1, "ARM",                     // Implementer
     0x41, 0x810, "ARM810",                  // Parts ...
     0x41, 0x920, "ARM920",
     0x41, 0x922, "ARM922",
@@ -171,7 +177,7 @@ static struct {
     0x41, 0xc07, "Cortex-A7",
     0x41, 0xc08, "Cortex-A8",
     0x41, 0xc09, "Cortex-A9",
-    0x41, 0xc0d, "Cortex-A17",              // # Originally A12
+    0x41, 0xc0d, "Cortex-A12/A17",          // Originally A12
     0x41, 0xc0f, "Cortex-A15",
     0x41, 0xc0e, "Cortex-A17",
     0x41, 0xc14, "Cortex-R4",
@@ -264,21 +270,16 @@ static struct {
     0x69, 0x688, "PXA30x",
     0x69, 0x689, "PXA31x",
     0x69, 0xb11, "SA1110",
-    0x69, 0xc12, "IPX1200",
-    -1,   -1,    ""                         // Mark end of table
+    0x69, 0xc12, "IPX1200"
 };
 
-static int get_part_index(int imp, int part) {
-    int i = 0;
-
-    // Loop until end of table
-    while (part_info[i].imp != -1 || part_info[i].part != -1) {
+static int get_part_info_index(int implementer, int partnum) {
+    for (int i = 0; i < elementsof(part_info); ++i ) {
         // Return if we have a match
-        if (part_info[i].imp == imp && part_info[i].part == part) {
+        if (   part_info[i].implementer == implementer
+            && part_info[i].partnum     == partnum) {
             return i;
         }
-
-        ++i;
     }
 
     // Indicate no match
@@ -286,7 +287,7 @@ static int get_part_index(int imp, int part) {
 }
 
 static struct {
-    int         arch;
+    int         architecture;
     const char *name;
 } arch_info[] = {
     0b0001, "Armv4",
@@ -295,21 +296,15 @@ static struct {
     0b0100, "Armv5T",
     0b0101, "Armv5TE",
     0b0110, "Armv5TEJ",
-    0b0111, "Armv6",
-    -1,     ""                              // Mark end of table
+    0b0111, "Armv6"
 };
 
-static int get_arch_index(int arch) {
-    int i = 0;
-
-    // Loop until end of table
-    while (arch_info[i].arch != -1) {
+static int get_architecture_index(int architecture) {
+    for (int i = 0; i < elementsof(arch_info); ++i ) {
         // Return if we have a match
-        if (arch_info[i].arch == arch) {
+        if (arch_info[i].architecture == architecture) {
             return i;
         }
-
-        ++i;
     }
 
     // Indicate no match
@@ -322,7 +317,7 @@ static int get_arch_index(int arch) {
 
 #if ANY_X64
 
-// -------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 // Return values from the cpuid instruction.
 // Create a union so that regardless of API we can always access
 // individual members as cpu.eax, etc.
@@ -336,7 +331,7 @@ typedef union {
 
 
 
-// -------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 // Make sure we have the proper level of CPU functionality
 
 static bool has_cpuid_level(int eax, int level) {
@@ -359,7 +354,7 @@ static bool has_cpuid_level(int eax, int level) {
 
 
 
-// -------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 // Get features from CPUID using the EAX ECX pair
 
 static bool get_cpu_functionality(cpu_regs *cpu, int eax, int ecx) {
@@ -385,7 +380,7 @@ static bool get_cpu_functionality(cpu_regs *cpu, int eax, int ecx) {
 
 
 
-// -------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 // Check CPU functionality
 //
 // https://en.wikipedia.org/wiki/AVX-512
@@ -414,6 +409,7 @@ bool cpu_has_avx512_ifma_vbmi(void) {
     if ((cpu.ecx & (1 << 1)) == 0)
         return false;
     
+    // Check prerequisites
     return cpu_has_avx512_f_cd();
 
 #endif
@@ -448,6 +444,7 @@ bool cpu_has_avx512_vl_dq_bw(void) {
     if ((cpu.ebx & (1 << 30)) == 0)
         return false;
     
+    // Check prerequisites
     return cpu_has_avx512_f_cd();
 
 #endif
@@ -478,6 +475,7 @@ bool cpu_has_avx512_er_pf(void) {
     if ((cpu.ebx & (1 << 26)) == 0)
         return false;
     
+    // Check prerequisites
     return cpu_has_avx512_f_cd();
 
 #endif
@@ -508,6 +506,7 @@ bool cpu_has_avx512_f_cd(void) {
     if ((cpu.ebx & (1 << 28)) == 0)
         return false;
 
+    // Check prerequisites
     return is_cpu_gen_4();
 
 #endif
@@ -565,6 +564,7 @@ bool is_cpu_gen_4(void) {
     if ((cpu.ecx & (1 << 5)) == 0)
         return false;
     
+    // Check prerequisites
     return cpu_has_avx2();
 
 #endif
@@ -591,6 +591,7 @@ bool cpu_has_avx2(void) {
     if ((cpu.ebx & (1 << 5)) == 0)
         return false;
 
+    // Check prerequisites
     return cpu_has_avx();
 
 #endif
@@ -617,6 +618,7 @@ bool cpu_has_avx(void) {
     if ((cpu.ecx & (1 << 28)) == 0)
         return false;
 
+    // Check prerequisites
     return cpu_has_sse4_2();
 
 #endif
@@ -659,6 +661,7 @@ bool cpu_has_sse4_2(void) {
     if ((cpu.ecx & (1 << 1)) == 0)
         return false;
 
+    // Check prerequisites
     return cpu_has_sse3();
 
 #endif
@@ -701,6 +704,7 @@ bool cpu_has_sse3(void) {
     if ((cpu.edx & (1 << 23)) == 0)
         return false;
 
+    // Check prerequisites
     return true;
 
 #endif
@@ -710,14 +714,20 @@ bool cpu_has_sse3(void) {
 
 
 
-// -------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 // Get CPU info using Linux's /proc/cpuinfo
+//
+// Note that the FILE * is handled outside of these functions.
+// This allows multple calls to continue the search from the last
+// match. Unless the caller uses fseek().
+//
+// Note the use of a static char array, this code is not thread safe
 
 #if defined(__linux__)                      // Linux
 
 static char *get_proc_cpuinfo_entry(FILE *fp, char *entry) {
     if (fp != NULL && entry != NULL) {
-        static char buf[2048];
+        static char buf[2048];              // Not thread safe
         
         while (fgets(buf, sizeof(buf), fp) != NULL) {
             size_t size = strlen(entry);
@@ -752,6 +762,8 @@ static int get_proc_cpuinfo_value(FILE *fp, char *entry, int base, int error) {
         }
     }
     
+    // Let caller specify the error code.
+    // We don't know what the valid values are for this entry.
     return error;
 }
 
@@ -759,7 +771,7 @@ static int get_proc_cpuinfo_value(FILE *fp, char *entry, int base, int error) {
 
 
 
-// -------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 // Identify the CPU vendor
 
 #if ANY_X64
@@ -793,9 +805,10 @@ static bool get_cpu_vendor_intel(char *buffer, size_t len) {
 static bool get_cpu_vendor_arm(char *buffer, size_t len, int implementer) {
     if (implementer > 0) {
         // Convert implementer value to a table index
-        implementer = get_part_index(implementer, -1);
+        implementer = get_part_info_index(implementer, -1);
         if (implementer > -1) {
-            return snprintf(buffer, len, "%s", part_info[implementer].name) > 0;
+            return   snprintf(buffer, len, "%s", part_info[implementer].name)
+                   > 0;
         }
     }
     
@@ -818,6 +831,7 @@ static bool verify_buffer(char *buffer, size_t len) {
 }
 
 
+
 bool get_cpu_vendor(char *buffer, size_t len) {
     if (! verify_buffer(buffer, len))
         return false;
@@ -838,7 +852,8 @@ bool get_cpu_vendor(char *buffer, size_t len) {
     FILE *fp      = fopen("/proc/cpuinfo", "r");
 
     if (fp != NULL) {
-        int implementer = get_proc_cpuinfo_value(fp, "CPU implementer", 16, -1);
+        int implementer = get_proc_cpuinfo_value(fp, "CPU implementer",
+                                                 16, -1);
         
         success = get_cpu_vendor_arm(buffer, len, implementer);
         
@@ -854,7 +869,7 @@ bool get_cpu_vendor(char *buffer, size_t len) {
 
 
 
-// -------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 // Identify the CPU brand
 
 #if ANY_X64
@@ -915,7 +930,8 @@ bool get_cpu_brand(char *buffer, size_t len) {
 #elif defined(__APPLE__)                    // macOS
 
     size_t size = len;
-    if (sysctlbyname("machdep.cpu.brand_string", buffer, &size, NULL, 0) == 0) {
+    if (   sysctlbyname("machdep.cpu.brand_string", buffer, &size, NULL, 0)
+        == 0) {
         strterm(buffer, len);
         
         return true;
@@ -945,10 +961,12 @@ bool get_cpu_brand(char *buffer, size_t len) {
 
 
 
-// -------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 // Identify the CPU part
 
 #if ANY_X64
+
+// https://en.wikipedia.org/wiki/CPUID
 
 static uint32_t get_cpu_family(uint32_t family, uint32_t exfamily) {
     if (family == 15)
@@ -957,7 +975,8 @@ static uint32_t get_cpu_family(uint32_t family, uint32_t exfamily) {
     return family;
 }
 
-static uint32_t get_cpu_model(uint32_t model, uint32_t exmodel, uint32_t family) {
+static uint32_t get_cpu_model(uint32_t model, uint32_t exmodel,
+                              uint32_t family) {
     if (family == 6 || family == 15)
         model += exmodel << 4;
 
@@ -1000,7 +1019,7 @@ static bool get_cpu_part_arm(char *buffer, size_t len,
                              int implementer, int partnum) {
     if (implementer > 0 && partnum > 0) {
         // Convert part number value to a table index
-        partnum = get_part_index(implementer, partnum);
+        partnum = get_part_info_index(implementer, partnum);
         if (partnum > -1) {
             return snprintf(buffer, len, "%s", part_info[partnum].name) > 0;
         }
@@ -1029,13 +1048,13 @@ bool get_cpu_part(char *buffer, size_t len) {
     if (info.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_ARM64) {
         bool v8 = IsProcessorFeaturePresent(PF_ARM_V8_INSTRUCTIONS_AVAILABLE);
         
-        snprintf(buffer, len, "ARM %s", v8 ? "v8" : "");
-        
-        return true;
+        return snprintf(buffer, len, "ARM %s", v8 ? "v8" : "") > 0;
     }
     
 #elif LINUX_64
 
+    // https://developer.arm.com/documentation/ddi0601/2025-03/AArch64-Registers/MIDR-EL1--Main-ID-Register
+    //
     // mrs x0, MIDR_EL1
     // Implementer, bits [31:24]
     // PartNum,     bits [15:4]
@@ -1051,8 +1070,10 @@ bool get_cpu_part(char *buffer, size_t len) {
     FILE *fp      = fopen("/proc/cpuinfo", "r");
     
     if (fp != NULL) {
-        int implementer = get_proc_cpuinfo_value(fp, "CPU implementer", 16, -1),
-            partnum     = get_proc_cpuinfo_value(fp, "CPU part",        16, -1);
+        int implementer = get_proc_cpuinfo_value(fp, "CPU implementer",
+                                                 16, -1),
+            partnum     = get_proc_cpuinfo_value(fp, "CPU part",
+                                                 16, -1);
         
         success = get_cpu_part_arm(buffer, len, implementer, partnum);
         
@@ -1068,7 +1089,7 @@ bool get_cpu_part(char *buffer, size_t len) {
 
 
 
-// -------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 // Identify the CPU cores
 
 bool get_cpu_cores(char *buffer, size_t len) {
@@ -1085,9 +1106,9 @@ bool get_cpu_cores(char *buffer, size_t len) {
 #elif defined(__APPLE__)                    // macOS
     
     int64_t cores = 0;
+    size_t  size  = sizeof(cores);
     
-    size_t size = sizeof(cores);
-    if (sysctlbyname("machdep.cpu.core_count", &cores,  &size, NULL, 0) == 0) {
+    if (sysctlbyname("machdep.cpu.core_count", &cores, &size, NULL, 0) == 0) {
         return snprintf(buffer, len, "%lld-Core", cores) > 0;
     }
     
@@ -1097,7 +1118,7 @@ bool get_cpu_cores(char *buffer, size_t len) {
     FILE *fp      = fopen("/proc/cpuinfo", "r");
     
     if (fp != NULL) {
-        long  cores = -1;
+        long cores = -1;
 
 #if defined(__x86_64__)                     // 64-bit Intel
 
@@ -1108,6 +1129,7 @@ bool get_cpu_cores(char *buffer, size_t len) {
         // There is no explicit count field, so count off processors
         do {
             int value = get_proc_cpuinfo_value(fp, "processor", 10, -1);
+            
             if (value >= 0) {
                 cores = value + 1;
             }
@@ -1134,7 +1156,7 @@ bool get_cpu_cores(char *buffer, size_t len) {
 
 
 
-// -------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 // Identify the CPU features
 
 #if ANY_X64
@@ -1184,30 +1206,61 @@ bool get_cpu_features_intel(char *buffer, size_t len) {
 
 #if defined(__APPLE__)                      // macOS
 
-static bool get_cpu_features_apple(char *features, int i) {
-    if (features != NULL) {
-        char name[64];
-        char buf[32];
-        
-        snprintf(name, sizeof(name), "hw.perflevel%d.name", i);
-        
-        size_t size = sizeof(buf);
-        if (sysctlbyname(name, buf, &size, NULL, 0) == 0) {
-            bufterm(buf);
+static bool get_cpu_core_decription_entry(char *features,
+                                          char *entry,
+                                          int   i,
+                                          bool  text) {
+    if (features == NULL || entry == NULL)
+        return false;
+    
+    char   name[64];
+    char   buffer[32];
+    size_t size;
 
-            strcat(features, buf);
-            strcat(features, ":");
+    // We need to construct a sysctl name that incorporates
+    // a count. There should be a "%d" in the entry string.
+    if (snprintf(name, sizeof(name), entry, i) > 0) {
+        // Handle a text based entry
+        if (text) {
+            size = sizeof(buffer);
+            if (sysctlbyname(name, buffer, &size, NULL, 0) == 0) {
+                bufterm(buffer);
+                strcat(features, buffer);
+                
+                return true;
+            }
         }
-        
-        snprintf(name, sizeof(name), "hw.perflevel%d.physicalcpu", i);
+        // Handle a numeric based entry
+        else {
+            int64_t ret = 0;
+            size        = sizeof(ret);
+            if (   sysctlbyname(name, &ret, &size, NULL, 0) == 0
+                && snprintf(buffer, sizeof(buffer), "%lld", ret) > 0) {
+                strcat(features, buffer);
+                strcat(features, " ");
+                
+                return true;
+            }
+        }
+    }
 
-        int64_t ret = 0;
-        size        = sizeof(ret);
-        if (sysctlbyname(name, &ret, &size, NULL, 0) == 0) {
-            snprintf(buf, sizeof(buf), "%lld", ret);
+    return false;
+}
 
-            strcat(features, buf);
-            strcat(features, " ");
+static bool get_cpu_core_decription(char *features, int i) {
+    if (features != NULL) {
+        // Handle the core desciption portion of the output
+        if (get_cpu_core_decription_entry(features,
+                                          "hw.perflevel%d.name",
+                                          i,
+                                          true)) {
+            strcat(features, ":");
+            
+            // Handle the count portion of the output
+            return get_cpu_core_decription_entry(features,
+                                                 "hw.perflevel%d.physicalcpu",
+                                                 i,
+                                                 false);
         }
     }
     
@@ -1250,7 +1303,7 @@ bool get_cpu_features(char *buffer, size_t len) {
         int levels = (int) ret;
         
         for (int i = 0; i < levels; ++i) {
-            get_cpu_features_apple(features, i);
+            get_cpu_core_decription(features, i);
         }
     }
     
